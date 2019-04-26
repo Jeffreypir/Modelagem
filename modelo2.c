70d9d9c179b9208c9036a4304b1a1bd54d717e1c
Jeffreypir <jefferson1995mat@gmail.com>
2019-04-26 17:39:50 +0000

    Instabilidade corrigida, erro de segmentação para 2 milhões de interações

--- modelo2.c
+++ modelo2.c
@@ -8,6 +8,17 @@ float prod(float c, float g, float t){
 	return (c*t)*(g*t);
 }
 
+/* Definindo somatorio */
+float somatorio(float *x, int n){
+	float soma = 0.0;
+	int i;
+	for ( i = 0; i < n; i++){
+		soma+= x[i];
+	}
+	return soma;
+}
+
+
 /*Tomemos as seguintes definições:
  * ct é o custo de Geração térmica associado á unidade termoelétrica n do subsistema s, no estágio t (depedendo pode ser dias, meses,etc).
  * gt é o custo de Geração da unidade termoelétrica n do subsistema s, no estágio t (depedendo pode ser dias, meses, etc).
@@ -20,23 +31,18 @@ float prod(float c, float g, float t){
 /* Iniciando a função de custo futuro */
 int main(){ 
 	float x[] = {1,2,3,4},y[] = {1,1,1,1}, z[] = {2,3,6,9}, w[]= {1,3,5,7};
-	int t,soma,i,l,j = sizeof(x)/sizeof(x[0]);;
+	int t,i,l,j = sizeof(x)/sizeof(x[0]);;
 	printf ("Digite o valor de t(meses) para ser avaliado: ");
 	scanf("%d", &l); 
 	float valor_final[l]; 
-	int vetor[j];
+	float vetor[j];
 	for (t = 0; t < l; t ++){
 		for (i = 0;i < j; i++){
 			vetor[i] = prod(x[i],y[i],t) + z[i] + w[i];
 		}
-	soma = 0;
-		for (i = 0; i < j ; i++){
-			soma = soma + vetor[i];
-		}
-	valor_final[t] = soma;
-	soma = 0;
+		valor_final[t] = somatorio(vetor,j);
 	}
-	for ( i = 0; i < l ; i++){
+	for (i = 0; i < l ; i++){
 		printf ( " %f \n", valor_final[i]);
 	}
 	return 0;
