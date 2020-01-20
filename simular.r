471e3bbeb54451393303a9274353b7ab8a2fe038
Jefferson Bezerra dos Santos <jefferson@debian>
2020-01-20 18:30:00 -0300

    Atualizando simular.r

--- simular.r
+++ simular.r
@@ -8,9 +8,9 @@ G1 <- unlist (read.table("G1.dat"))
 G2 <- unlist (read.table("G2.dat"))
 
 # Arquivo plotagem em formato eps
-cairo_ps("simulacaopb01pt10.eps",5.0,5.0)
+cairo_ps("simula.eps",5.0,5.0)
 matplot (demanda,cbind(Hidr1,Hidr2,G1,G2),
-		 main ="Depacho Hidrotérmico 1",
+		 main ="Depacho Hidrotérmico",
 		 font.main=1,
 		 xlab="Demanda (MW)",
 		 col = c("blue","green","red","yellow"), 
@@ -38,9 +38,9 @@ legend (100000,35000,inset=0.01,
 	   	horiz=F)
 dev.off()
 
-pdf("simulacaopb01pt10.pdf",5.0,5.0)
+pdf("simula.pdf",5.0,5.0)
 matplot (demanda,cbind(Hidr1,Hidr2,G1,G2),
-		 main ="Depacho Hidrotérmico 1",
+		 main ="Depacho Hidrotérmico",
 		 font.main=1,
 		 xlab="Demanda (MW)",
 		 col = c("blue","green","red","yellow"), 
